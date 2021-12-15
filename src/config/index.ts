6344c5aa6ece80210fa6ce8efc3f77850a7b6642
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-12-15 15:34:09 -0300

    Change main file on package.json

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export const LIMIT_FILE_SIZE = 10 * 1024;
+export const LIMIT_FILE_SIZE = 10 * 1024 * 1024;
 
 export const TMP_PATH = path.join(__dirname, '..', '..', 'tmp');
 
