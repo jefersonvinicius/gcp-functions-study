ba67437e791ee4c456f61f8328befa86041608ce
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-12-15 16:03:08 -0300

    Update cloudbuild.yaml

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ const uploadController = new UploadController();
 
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, '/../uploads')));
-
 app.post('/upload', uploadController.handle);
 
 export default app;
