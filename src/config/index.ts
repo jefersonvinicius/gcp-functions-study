13abda5aa96b6d14f278c0dd2467f5399dfdb486
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-12-16 18:09:34 -0300

    Use OS tmp folder

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
+import { tmpdir } from 'os';
 import path from 'path';
 
 export const LIMIT_FILE_SIZE = 10 * 1024 * 1024;
 
-export const TMP_PATH = path.join(__dirname, '..', '..', 'tmp');
+export const TMP_PATH = tmpdir();
 
 export const UPLOADS_PATH = path.join(__dirname, '..', '..', 'uploads');
